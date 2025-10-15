6e62609c0b95a21e18e3b42722d25ad0b1ed72ae
Dmytro Ivanov <ivanoffdn@gmail.com>
2025-10-15 17:50:05 +0300

    added semantic_segmentation.py

---
+++ semantic_segmentation.py
@@ -0,0 +1,57 @@
+# from ransac_numpy import StructuralRANSAC
+import matplotlib.pyplot as plt
+import numpy as np
+import open3d as o3d
+
+
+class SemanticSegmentor:
+    def __init__(self, s3dis_validator, area, room, subsample_method, ransac_segmentor):
+        # self.s3dis_validator = s3dis_validator
+        self.classes = {
+            "ceiling": 0,
+            "floor": 1,
+            "wall": 2,
+            "beam": 3,
+            "column": 4,
+            "window": 5,
+            "door": 6,
+            "table": 7,
+            "chair": 8,
+            "sofa": 9,
+            "bookcase": 10,
+            "board": 11,
+            "clutter": 12,
+        }
+        self.area = area
+        self.room = room
+        self.subsample_method = subsample_method
+        self.points, self.labels = s3dis_validator.load_and_process_room(
+            self.area, self.room, self.subsample_method
+        )
+
+        self.pcd = self._create_pcd()
+
+        self.ransac_segmentor = ransac_segmentor(self.pcd, downsample=False)
+        self.ransac_segmentor.segment()
+
+    def _create_pcd(self):
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(self.points[:, :3])
+        pcd.colors = o3d.utility.Vector3dVector(self.points[:, 3:])
+        return pcd
+
+    def map_ransac_to_s3dis(
+        self,
+    ):
+        """
+        Map RANSAC segments to S3DIS label format
+        """
+        point_labels = np.full(len(self.labels), -1)  # -1 for unlabeled
+
+        for segment_type, data in self.ransac_segmentor.segments.items():
+            # Parse segment type (e.g., 'wall_1' -> 'wall')
+            base_type = segment_type.split("_")[0]
+
+            if base_type in self.classes:
+                point_labels[data["indices"]] = self.classes[base_type]
+        return point_labels
